6e30557a42ad0c4f9c57a42560169d30f807fd54
best-time-is-now <n.develop.mail@gmail.com>
2023-09-11 02:42:40 +0300

    One random element is chosen to run the Scenario

--- helpers/fileReader.js
+++ helpers/fileReader.js
@@ -9,10 +9,8 @@ module.exports = {
         return string.split("\r\n").map(Number);
     },
 
-    addRandomElement2Array(array) {
+    getRandomElement2Array(array) {
         const randomIndex = Math.floor(Math.random() * array.length);
-        const randomElement = array[randomIndex];
-        array.push(randomElement);
-        return array;
+        return array[randomIndex];
     }
 };
--- tests/buyProduct_test.js
+++ tests/buyProduct_test.js
@@ -3,7 +3,6 @@ const FileReader = require('../helpers/fileReader.js');
 const PATH = './productIds.txt';
 const fileAsString = FileReader.readFile(PATH);
 let productIds = FileReader.convertStringToArray(fileAsString);
-//productIds = FileReader.addRandomElement2Array(productIds);
 
 const USER = {
     email: "114@test.com",
@@ -23,8 +22,7 @@ Before(({ I }) => {
     I.login(USER);
 });
 
-Data(productIds).Scenario('buy product', async ({ I, productPage, cartPage, current }) => {
-    
+Data([FileReader.getRandomElement2Array(productIds)]).Scenario('buy product', async ({ I, productPage, cartPage, current }) => {
     I.amOnPage('/index.php?route=product/product&product_id=' + current);
     await I.emptyCart();
     await productPage.selectColor();
@@ -43,3 +41,4 @@ Data(productIds).Scenario('buy product', async ({ I, productPage, cartPage, curr
     }
 
 }).tag("buy");
+
